3b2e0ffa46ca1954d337a80ef8095b2b3720abba
NarmadhaDevarajan <narmi.jan4@gmail.com>
2023-07-13 21:05:58 +0530

    api end point

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 //simple web server
 const express=require('express') ;
+// const ATLAS_URL=require('ATLAS_URL');
 const app=express();
 const cors = require('cors');
 //middleware
 app.use(cors());
 
 app.use(express.json());
+const url=process.env.ATLAS_URL;
 const mongoose=require("mongoose");
 // console.log(process.argv);
-const url=`mongodb+srv://narmijan4:narmathajan4@cluster0.yq6ncdj.mongodb.net/?retryWrites=true&w=majority`;
+// const url=`mongodb+srv://narmijan4:narmathajan4@cluster0.yq6ncdj.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(url)
                 .then(()=>{
                     console.log("connected to mongodb");
                     // mongoose.connection.close();
                 })
-                .catch((err)=>{
-                    console.error(err);
+                .catch((error)=>{
+                    console.log(error);
                 });
 //save a note in db
 //define a schema
@@ -44,36 +46,46 @@ app.get('/api/notes',(request,response)=>
     .then(notes=>{response.status(200).json(notes)});
 })
 // to create new resource based on request data
-// app.post('/api/notes',(request,response)=>{
-//     // console.log(request.body);
-//     notes=notes.concat(request.body);
-//     // response.status(201).json({message:'post request made succesful'})
-//     response.status(201).json({message:'note created successfully'});
-// });
+app.post('/api/notes',(request,response)=>{
+    // console.log(request.body);
+    notes=notes.concat(request.body);
+    // response.status(201).json({message:'post request made succesful'})
+    note.save()
+    .then(()=>{
+        response.status(201).json({message:'note created successfully'});
+    })
+    
+});
 // //fetches a single resource based on id
-//     app.get('/api/notes/:id',(request,response)=>{
-//     const id=request.params.id;
-//     const note=notes.find(note=>note.id==id);
-//     if(note){
-//         response.status(200).json(note);
-//     }
-//     else{
-//     response.status(404).json({message:"id does not exist "});}
-// });
+    app.get('/api/notes/:id',(request,response)=>{
+    const id=request.params.id;
+    Note.findById(id)
+    .then(note=>{
+        if(note){
+            response.status(200).json(note);
+        }
+        else{
+            response.status(404).json({message:"id does not exist "});}
+        })}
+    );
+    
 
 // //delete a single resource based on ID
-// app.delete('/api/notes/:id',(request,response)=>{
-//     //get th id
-//     const id=request.params.id;
-//     // notes.filter(note=>note.id!=id);
-//     const note=notes.find(note=>note.id==id);
-//     notes=notes.filter(note=>note.id != id);
-//     if(note){
-//         response.status(204).json(note);
-//     }
-//     else{
-//     response.status(404).json({message:"id does not exist "});}
-// })
+app.delete('/api/notes/:id',(request,response)=>{
+    //get th id
+    const id=request.params.id;
+    // notes.filter(note=>note.id!=id);
+    Note.findByIdAndDelete(id)
+    .then((deletedNote)=>{
+        if(deletedNote){
+            response.status(204).json(note);
+        }
+        else{
+            response.status(404).json({message:"id does not exist "});
+        }
+    });
+    });
+    
 // //replace the entire note object identified by an id
 // app.put('api/notes/:id',(request,response)=>
 // {
@@ -101,4 +113,5 @@ app.get('/api/notes',(request,response)=>
 // });
 
 const PORT=3001;
-app.listen(PORT,()=>console.log(`Server running on the port number ${PORT}`));
+app.listen(PORT,()=>{
+        console.log(`Server running on the port number ${PORT}`)});
